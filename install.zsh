d6b02836b4079a647b85147580c54add1c6f8ba8
Pratham Patel <thefirst1322@gmail.com>
2020-08-12 15:39:00 +0530

    init commit

---
+++ install.zsh
@@ -0,0 +1,26 @@
+#!/usr/bin/zsh
+
+
+# Ensure that the system clock is accurate
+timedatectl set-ntp true
+
+
+# Check EFI boot
+if [ "$(ls -A /sys/firmware/efi/efivars)" ]
+then
+	echo "" > /dev/null
+else
+	echo "The installer did not start in EFI mode."
+	read tmp
+fi
+
+# List drives and let user identify which drive to use
+clear
+fdisk -l
+
+echo "Please input the full device path where you want Arch:"
+read OS_DRIVE
+
+clear
+fdisk -l "$OS_DRIVE"
+echo -e "\n\nIs this your drive?"
