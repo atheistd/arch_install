8ef2e2aaedbde2db0bc26c93be580cd9bb386a08
Pratham Patel <thefirst1322@gmail.com>
2020-09-07 18:41:00 +0530

    added xterm

--- xsys.zsh
+++ xsys.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-sudo pacman -Sy xorg xorg-xinit ttf-fira-code nitrogen picom terminator firefox base-devel awesome
+sudo pacman -Sy xorg xorg-xinit ttf-fira-code nitrogen picom terminator xterm firefox base-devel awesome
 
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
