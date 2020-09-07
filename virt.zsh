592784f7436a2a596cffa310e4f4f91e058ab7f8
Pratham Patel <thefirst1322@gmail.com>
2020-09-07 13:45:16 +0530

    new branch for vm

---
+++ virt.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/zsh
+
+fdisk /dev/sda << EOF
+g
+n
+1
+
++550M
+n
+2
+
+
+t
+1
+1
+w
+EOF
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+
+pacstrap /mnt base linux linux-firmware git vim nano less man dhcpcd sudo pacman-contrib reflector zsh grub efibootmgr os-prober openssh
+
+genfstab -U /mnt >> /mnt/etc/fstab
