93d00436f31cc3738a964949a3eb21f512a63c2e
Pratham Patel <thefirst1322@gmail.com>
2020-09-09 23:39:26 +0530

    removed polybar from pacman

--- xsys.zsh
+++ xsys.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
-sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu polybar pcmanfm picom terminator xterm firefox awesome vlc
+sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc
 
 yay -S polybar
