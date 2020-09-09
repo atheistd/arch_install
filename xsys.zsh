c7875f56d9de8ac63069230c5a6398afed7fd33e
Pratham Patel <thefirst1322@gmail.com>
2020-09-09 22:52:23 +0530

    forgot to add xorg-server _facepalm_

--- xsys.zsh
+++ xsys.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
-sudo pacman -Sy xorg xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu polybar pcmanfm picom terminator xterm firefox base-devel awesome vlc
+sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu polybar pcmanfm picom terminator xterm firefox base-devel awesome vlc
 
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git/
