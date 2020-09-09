5f2cc3cb741910b3e67b85e85ec357ba2e573ed8
Pratham Patel <thefirst1322@gmail.com>
2020-09-09 23:42:06 +0530

    scripted xinit and awesome initial setup

--- xsys.zsh
+++ xsys.zsh
@@ -3,3 +3,8 @@
 sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc
 
 yay -S polybar
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+mkdir -p ~/.config/awesome/
+cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
+vim ~/.xinitrc
