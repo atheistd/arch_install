11c29122837779ca0a814022f9442fb5718fed58
Pratham Patel <thefirst1322@gmail.com>
2021-05-27 17:00:18 +0530

    added a few packages to be installed on the base system

--- base_packages.zsh
+++ base_packages.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-pacstrap /mnt base linux linux-firmware git vim nano less man dhcpcd sudo pacman-contrib reflector zsh fish grub efibootmgr os-prober openssh base-devel
+pacstrap /mnt base base-devel cmatrix curl dhcpcd efibootmgr ffmpeg fish git grub htop iotop iperf less linux-firmware linux-lts man nano neofetch nload openssh os-prober pacman-contrib reflector rsync sudo unzip vim wget zsh
