163dbc3130121b770a9d897229ab7257455aa042
Pratham Patel <thefirst1322@gmail.com>
2021-05-30 23:25:32 +0530

    replaced awesome with i3

--- xsys.zsh
+++ xsys.zsh
@@ -1,20 +1,20 @@
 #!/usr/bin/env zsh
 
-sudo pacman -Syy
-sudo pacman -Syu
-sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc --noconfirm --needed
-
-#yay -S polybar
-
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-mkdir -p ~/.config/awesome/
-cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
-vim ~/.xinitrc
-
 cd /tmp/
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
+
+sudo pacman -Syy
+sudo paru -Syu
+sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc i3 --noconfirm --needed
+
+#sudo paru -S polybar
+
+cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
+mkdir -p $HOME/.config/i3/
+cp /etc/i3/config $HOME/.config/i3/
+vim $HOME/.xinitrc
 
 #sudo pacman -S wayland sway swaylock swayidle terminator tmux lsof
