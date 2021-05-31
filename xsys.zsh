d468020ca9285f55d65cb08d358eb6fcccb735b2
Pratham Patel <thefirst1322@gmail.com>
2021-05-31 13:41:36 +0530

    added lightdm-gtk-greeter package to xsys.zsh

--- xsys.zsh
+++ xsys.zsh
@@ -8,7 +8,7 @@ makepkg -si
 
 sudo pacman -Syy
 sudo paru -Syu
-sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc i3 --noconfirm --needed
+sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter lightdm-gtk-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc i3 --noconfirm --needed
 
 #sudo paru -S polybar
 
