4ed3dd3ba27958a1d2888ac5981bb3da6dc6e981
Pratham Patel <thefirst1322@gmail.com>
2021-05-31 14:40:54 +0530

    moved paru installation to a new script and added comments to xsys

--- xsys.zsh
+++ xsys.zsh
@@ -1,15 +1,15 @@
 #!/usr/bin/env zsh
 
-cd /tmp/
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-
 sudo pacman -Syy
 sudo paru -Syu
+
+# X11
 sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter lightdm-gtk-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox vlc i3 --noconfirm --needed
 
+# Wayland+X
+#sudo pacman -S wayland sway swaylock swayidle terminator tmux lsof
+
+# Extras
 #sudo paru -S polybar
 
 cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
@@ -17,4 +17,3 @@ mkdir -p $HOME/.config/i3/
 cp /etc/i3/config $HOME/.config/i3/
 vim $HOME/.xinitrc
 
-#sudo pacman -S wayland sway swaylock swayidle terminator tmux lsof
