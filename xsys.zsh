c143dbfd0cacc87e45f98de8e1176afea8b24f31
Pratham Patel <thefirst1322@gmail.com>
2021-05-31 13:50:59 +0530

    moved paru installation steps from xsys.zsh to chroot_setup.zsh

--- xsys.zsh
+++ xsys.zsh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
 
-cd /tmp/
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
-
 sudo pacman -Syy
 sudo paru -Syu
 sudo pacman -Sy xorg xorg-server xorg-xinit lightdm lightdm-webkit2-greeter lightdm-gtk-greeter ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox awesome vlc i3 --noconfirm --needed
