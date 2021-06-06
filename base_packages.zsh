fde3da22bc4d7ec0730fac76b0c61c64b38d26fb
Pratham Patel <thefirst1322@gmail.com>
2021-06-06 22:12:01 +0530

    added tmux to be installed on the base system

--- base_packages.zsh
+++ base_packages.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-pacstrap /mnt base base-devel cmatrix curl dhcpcd efibootmgr ffmpeg fish git grub htop iotop iperf less linux-firmware linux-lts man nano neofetch nload openssh os-prober pacman-contrib reflector rsync sudo unzip vim wget zsh --needed
+pacstrap /mnt base base-devel tmux cmatrix curl dhcpcd efibootmgr ffmpeg fish git grub htop iotop iperf less linux-firmware linux-lts man nano neofetch nload openssh os-prober pacman-contrib reflector rsync sudo unzip vim wget zsh --needed
