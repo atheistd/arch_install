f32deff0839aa63e6c3337be9435ae03c1dca936
Pratham Patel <thefirst1322@gmail.com>
2021-07-06 21:27:22 +0530

    added more packages on base system and more packages for wm too

--- xsys.zsh
+++ xsys.zsh
@@ -4,22 +4,5 @@ sudo pacman -Syy
 yay -Syu
 
 # X11
-sudo pacman -Sy xorg rofi xorg-server xorg-xinit ttf-fira-code nitrogen conky dmenu pcmanfm picom terminator xterm firefox vlc --noconfirm --needed
-#sudo pacman -Sy i3
-#sudo pacman -Sy bspwm
-
-
-# Wayland+X
-#sudo pacman -S wayland sway swaylock swayidle terminator tmux lsof
-
-# Extras
-yay -S polybar
-
-cp /etc/X11/xinit/xinitrc $HOME/.xinitrc
-vim $HOME/.xinitrc
-
-#mkdir -p $HOME/.config/bspwm/
-#cp /usr/share/doc/bspwm/examples/bspwmrc $HOME/.config/bspwm/
-
-#mkdir -p $HOME/.config/i3/
-#cp /etc/i3/config $HOME/.config/i3/
+sudo pacman -Sy breeze bspwm conky dmenu dunst feh firefox i3lock mpv nitrogen pcmanfm picom rofi sxhkd terminator ttf-fira-code ttf-font-awesome vlc xorg xorg-server xorg-xinit xterm --noconfirm --needed
+yay -S i3lock-fancy picom polybar google-chrome brave
