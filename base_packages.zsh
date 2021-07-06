f1c589a3db1a8f11276d2251dcf12c9e2661ad32
Pratham Patel <thefirst1322@gmail.com>
2021-07-06 21:31:56 +0530

    mistakenly added new packages in base_packages.zsh, remedied the mistake

--- base_packages.zsh
+++ base_packages.zsh
@@ -1,6 +1,3 @@
 #!/usr/bin/env zsh
 
 pacstrap /mnt aria2 base base-devel bash cmatrix curl dhcpcd efibootmgr ffmpeg flatpak git grub htop iotop iperf jq less libnotify libpam-google-authenticator linux-firmware linux-lts man nano neofetch nload openssh os-prober pacman-contrib reflector rsync socat sudo tmux unzip vim wget zsh -needed
-
-pacman -S breeze bspwm dunst feh firefox ttf-font-awesome ttf-fira-code i3lock sxhkd terminator mpv
-yay -S i3lock-fancy picom polybar google-chrome brave
